714e11e792d0d175c598f4df05cddef1a566f750
Vlastiss <120052173+Vlastiss@users.noreply.github.com>
2024-01-10 19:17:27 +0000

    Update script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+const display = document.getElementById('display');
+
+function appendToDisplay(symbol) {
+ 
+}
+
+function delteSymbol() {
+
+}
+
+function clearDisplay() {
+
+}
+
+function calculate() {
+
+}
