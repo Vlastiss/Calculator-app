9fe61d9ab28b9354345d4c49f573425015ec4bd4
Vlastiss <120052173+Vlastiss@users.noreply.github.com>
2024-01-10 19:45:51 +0000

    final version

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
 const display = document.getElementById('display');
 
 function appendToDisplay(symbol) {
- 
+  display.value += symbol;
 }
 
-function delteSymbol() {
-
+function deleteSymbol() {
+  display.value = display.value.slice(0, -1);
 }
 
 function clearDisplay() {
-
+  display.value = '';
 }
 
 function calculate() {
-
+  let result = eval(display.value);
+  display.value = parseFloat(result).toFixed(5);
 }
